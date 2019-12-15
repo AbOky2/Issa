4d323782cfaa3ad61ef60df97d6b2259eca04034
kacoulib <kacoulib@gmail.com>
2019-12-15 20:34:05 +0100

    Clean project

--- app/src/services/index.js
+++
@@ -1,8 +0,0 @@
-import { getToken } from '../utils/storage'
-
-const getRequestConfig = () => ({
-    headers: {
-        Authorization: "Bearer " + getToken()
-    }
-})
-export { getRequestConfig }
